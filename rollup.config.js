cbb6a9a9edb48275c0322b86c537af2eaf0352c2
Sahithyan Kandathasan <sahithyan2701@gmail.com>
2022-07-15 07:38:29 +0530

    build(deps): add rollup-cache

--- rollup.config.js
+++ rollup.config.js
@@ -1,7 +1,13 @@
-export default {
+import { cacheBuild } from "rollup-cache";
+
+const cacheConfig = {
+	name: "testing",
+};
+
+export default cacheBuild(cacheConfig, {
 	input: "src/main.js",
 	output: {
 		dir: "dist",
 		format: "iife",
 	},
-};
+});
